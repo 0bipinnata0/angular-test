3884918cdcd37cafd7f57f285aa16b80dfd44bba
tom <mail>
2022-10-15 23:32:49 +0800

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test';
+  length = 0
+  password = ""
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false;
+
+  onChangeUseLetter (){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = "0123456789"
+    const letters = "abcdefghijklmnopqrstuvwxyz"
+    const symbols = "!@#$%^&*()"
+    let validChars = "";
+    if(this.includeNumbers){
+      validChars+=numbers
+    }
+    if(this.includeLetters){
+      validChars+=letters;
+    }
+    if(this.includeSymbols){
+      validChars+=symbols;
+    }
+    let generatedPassword = ""
+    for(let i = 0;i<this.length;i++){
+      const index = Math.floor(Math.random()*validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeLength (value:string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
 }
