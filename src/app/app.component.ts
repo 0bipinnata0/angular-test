dec8584305a1455014b9838fab1bdf3258c1a391
tom <mail>
2022-10-16 10:26:15 +0800

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { faker } from '@faker-js/faker';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  randomText = faker.lorem.sentence();
+  enterText = '';
+
+  get show() {
+    return this.enterText === this.randomText;
+  }
+  onInput(value: string) {
+    this.enterText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
