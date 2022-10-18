b42f5b565b2b9c330930438d0fa197d470d00e9d
tom <mail>
2022-10-18 21:12:08 +0800

    module system

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ ng g m Elements --routing
 ng g m Collections --routing
 ng g m Views --routing
 ng g m Mods --routing
+
+ng g c elements/ElementsHome
+
+# modules Types
+Domain wraps up all the components needed to implement one single feature
+Routed Same as a 'Domain' module, but thesse components are tied to routes(only displayed when the user is at some particular address)
+Routing Defines routing (show TodoList component when a user is at a route of '/todos')
+Service Defines services that will be used in multiple parts of the app
+Widget Defines some reusable components that will be used in multiple other modules.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,14 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
