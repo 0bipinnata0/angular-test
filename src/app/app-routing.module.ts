d0d8128da0e2a8fedb7a1eb243a4643953d66d32
tom <mail>
2022-10-18 22:09:50 +0800

    elements和collections懒加载

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'elements',
+    loadChildren: () =>
+      import('./elements/elements.module').then((m) => m.ElementsModule),
+  },
+  {
+    path: 'collections',
+    loadChildren: () =>
+      import('./collections/collections.module').then(
+        (m) => m.CollectionsModule
+      ),
+  },
+  {
     path: '',
     component: HomeComponent,
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { CollectionsModule } from './collections/collections.module';
-import { ElementsModule } from './elements/elements.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,7 +9,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
   // 如果不调整次序，那么一旦进入notfound，那么再也无法进入其他的routing，所以AppRouting需要放在最后，防止通配符匹配其他项
-  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
