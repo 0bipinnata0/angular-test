204e8dff1d37e7dcea3071f72af7a591f92f25f9
tom <mail>
2022-10-19 08:20:22 +0800

    add external class name

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  @Input('class') classNames = '';
   @Input() data: Array<{ name: string; job: string; age: number }> = [];
   @Input() headers: Array<{ key: 'name' | 'job' | 'age'; label: string }> = [];
   constructor() {}
