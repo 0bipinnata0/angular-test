0ff94abb275424224b2a54908ac02c434d62ea54
tom <mail>
2022-10-26 22:33:48 +0800

    finish

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -6,9 +6,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent implements OnInit {
+  openedItemIndex = 0;
   constructor() {}
 
   ngOnInit(): void {}
 
   @Input() items: Array<{ title: string; content: string }> = [];
+  onClick(index: number) {
+    if (this.openedItemIndex === index) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = index;
+    }
+  }
 }
